019c83c84ba001cfd588a6bef07e675150ae0d98
NguyenHieu09 <hieu92145@gmail.com>
2024-06-08 00:20:29 +0700

    Developing homepage: added footer section and adjusted layout

---
+++ script.js
@@ -0,0 +1,58 @@
+// Function to load translation file
+function loadTranslationFile(language) {
+    return axios.get(`./languages/${language}.json`)
+        .then(response => response.data)
+        .catch(error => {
+            console.error(`Could not load ${language}.json.`);
+            console.error(error);
+        });
+}
+
+// Load translation files and initialize i18next
+Promise.all([loadTranslationFile('vi'), loadTranslationFile('ja')]).then(([viTranslation, jaTranslation]) => {
+    i18next.init({
+        lng: 'vi', // Default language
+        resources: {
+            vi: {
+                translation: viTranslation
+            },
+            ja: {
+                translation: jaTranslation
+            }
+        }
+    }, function (err, t) {
+        if (err) {
+            console.error('i18next initialization error:', err);
+        } else {
+            jqueryI18next.init(i18next, $);
+            $('body').localize();
+        }
+    });
+});
+
+// Add click event listeners for language change buttons
+$(document).ready(function () {
+    $('#change-to-vi').click(() => changeLanguage('vi'));
+    $('#change-to-ja').click(() => changeLanguage('ja'));
+});
+
+// Function to change language and update the UI
+function changeLanguage(language) {
+    // console.log('Changing language to:', language);
+    i18next.changeLanguage(language, () => {
+        $('body').localize();
+        // console.log('Language changed to:', language);
+    });
+}
+
+function showDropdown(element) {
+    var dropdownContent = element.querySelector('.dropdown-content');
+    dropdownContent.style.display = "block";
+}
+
+function hideDropdown(element) {
+    var dropdownContent = element.querySelector('.dropdown-content');
+    dropdownContent.style.display = "none";
+}
+
+
