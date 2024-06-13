e0a47773febba80796252be02743cbd2a0f9de70
NguyenHieu09 <hieu92145@gmail.com>
2024-06-13 20:36:57 +0700

    update form sign in, sign up

--- javascript/script.js
+++ javascript/script.js
@@ -1,49 +1,49 @@
-// Function to load translation file
-function loadTranslationFile(language) {
-    return axios.get(`./languages/${language}.json`)
-        .then(response => response.data)
-        .catch(error => {
-            console.error(`Could not load ${language}.json.`);
-            console.error(error);
-        });
-}
+// // Function to load translation file
+// function loadTranslationFile(language) {
+//     return axios.get(`./languages/${language}.json`)
+//         .then(response => response.data)
+//         .catch(error => {
+//             console.error(`Could not load ${language}.json.`);
+//             console.error(error);
+//         });
+// }
 
-// Load translation files and initialize i18next
-Promise.all([loadTranslationFile('vi'), loadTranslationFile('ja')]).then(([viTranslation, jaTranslation]) => {
-    i18next.init({
-        lng: 'vi', // Default language
-        resources: {
-            vi: {
-                translation: viTranslation
-            },
-            ja: {
-                translation: jaTranslation
-            }
-        }
-    }, function (err, t) {
-        if (err) {
-            console.error('i18next initialization error:', err);
-        } else {
-            jqueryI18next.init(i18next, $);
-            $('body').localize();
-        }
-    });
-});
+// // Load translation files and initialize i18next
+// Promise.all([loadTranslationFile('vi'), loadTranslationFile('ja')]).then(([viTranslation, jaTranslation]) => {
+//     i18next.init({
+//         lng: 'vi', // Default language
+//         resources: {
+//             vi: {
+//                 translation: viTranslation
+//             },
+//             ja: {
+//                 translation: jaTranslation
+//             }
+//         }
+//     }, function (err, t) {
+//         if (err) {
+//             console.error('i18next initialization error:', err);
+//         } else {
+//             jqueryI18next.init(i18next, $);
+//             $('body').localize();
+//         }
+//     });
+// });
 
-// Add click event listeners for language change buttons
-$(document).ready(function () {
-    $('#change-to-vi').click(() => changeLanguage('vi'));
-    $('#change-to-ja').click(() => changeLanguage('ja'));
-});
+// // Add click event listeners for language change buttons
+// $(document).ready(function () {
+//     $('#change-to-vi').click(() => changeLanguage('vi'));
+//     $('#change-to-ja').click(() => changeLanguage('ja'));
+// });
 
 
-function changeLanguage(language) {
+// function changeLanguage(language) {
 
-    i18next.changeLanguage(language, () => {
-        $('body').localize();
+//     i18next.changeLanguage(language, () => {
+//         $('body').localize();
 
-    });
-}
+//     });
+// }
 
 function showDropdown(element) {
     var dropdownContent = element.querySelector('.dropdown-content');
