da5d972cf722a7d4060570da148c540c7f573f15
NguyenHieu09 <hieu92145@gmail.com>
2024-06-21 21:43:29 +0700

    complete login, logout

--- javascript/script.js
+++ javascript/script.js
@@ -1,143 +1,53 @@
-// // Function to load translation file
-// function loadTranslationFile(language) {
-//     return axios.get(`./languages/${language}.json`)
-//         .then(response => response.data)
-//         .catch(error => {
-//             console.error(`Could not load ${language}.json.`);
-//             console.error(error);
-//         });
-// }
+// // // Function to load translation file
+// // function loadTranslationFile(language) {
+// //     return axios.get(`./languages/${language}.json`)
+// //         .then(response => response.data)
+// //         .catch(error => {
+// //             console.error(`Could not load ${language}.json.`);
+// //             console.error(error);
+// //         });
+// // }
 
-// // Load translation files and initialize i18next
-// Promise.all([loadTranslationFile('vi'), loadTranslationFile('ja')]).then(([viTranslation, jaTranslation]) => {
-//     i18next.init({
-//         lng: 'vi', // Default language
-//         resources: {
-//             vi: {
-//                 translation: viTranslation
-//             },
-//             ja: {
-//                 translation: jaTranslation
-//             }
-//         }
-//     }, function (err, t) {
-//         if (err) {
-//             console.error('i18next initialization error:', err);
-//         } else {
-//             jqueryI18next.init(i18next, $);
-//             $('body').localize();
-//         }
-//     });
-// });
+// // // Load translation files and initialize i18next
+// // Promise.all([loadTranslationFile('vi'), loadTranslationFile('ja')]).then(([viTranslation, jaTranslation]) => {
+// //     i18next.init({
+// //         lng: 'vi', // Default language
+// //         resources: {
+// //             vi: {
+// //                 translation: viTranslation
+// //             },
+// //             ja: {
+// //                 translation: jaTranslation
+// //             }
+// //         }
+// //     }, function (err, t) {
+// //         if (err) {
+// //             console.error('i18next initialization error:', err);
+// //         } else {
+// //             jqueryI18next.init(i18next, $);
+// //             $('body').localize();
+// //         }
+// //     });
+// // });
 
-// // Add click event listeners for language change buttons
-// $(document).ready(function () {
-//     $('#change-to-vi').click(() => changeLanguage('vi'));
-//     $('#change-to-ja').click(() => changeLanguage('ja'));
-// });
-
-
-// function changeLanguage(language) {
-
-//     i18next.changeLanguage(language, () => {
-//         $('body').localize();
-
-//     });
-// }
+// // // Add click event listeners for language change buttons
+// // $(document).ready(function () {
+// //     $('#change-to-vi').click(() => changeLanguage('vi'));
+// //     $('#change-to-ja').click(() => changeLanguage('ja'));
+// // });
 
 
+// // function changeLanguage(language) {
 
-// $(document).ready(function () {
-//     $("#navbar-container").load("nav.html");
-// });
+// //     i18next.changeLanguage(language, () => {
+// //         $('body').localize();
 
-// function showDropdown(element) {
-//     var dropdownContent = element.querySelector('.dropdown-content');
-//     dropdownContent.style.display = "block";
-// }
-
-// function hideDropdown(element) {
-//     var dropdownContent = element.querySelector('.dropdown-content');
-//     dropdownContent.style.display = "none";
-// }
+// //     });
+// // }
 
 
 
 $(document).ready(function () {
-    // Load and open the signup modal when the link in the signin modal is clicked
-    $(document).on('click', '#openSignupFromSignin', function () {
-        $('#loginModal').modal('hide');
-        $('#modal-container').load('signUp.html #registerModal', function () {
-            $('#registerModal').modal('show');
-        });
-    });
-
-    // Load and open the signin modal when the link in the signup modal is clicked
-    $(document).on('click', '#openSigninFromSignup', function () {
-        $('#registerModal').modal('hide');
-        $('#modal-container').load('signIn.html #loginModal', function () {
-            $('#loginModal').modal('show');
-        });
-    });
-});
-
-
-$(document).ready(function () {
-    $('#openSignupModal').click(function () {
-        $('#modal-container').load('signUp.html #registerModal', function () {
-            $('#registerModal').modal('show');
-
-            // Thêm event listener sau khi form đăng ký được tải và hiển thị
-            document.querySelector('#registerForm').addEventListener('submit', function (event) {
-                // Ngăn chặn hành vi mặc định của form
-                event.preventDefault();
-
-                // Lấy dữ liệu từ form
-                const firstName = document.querySelector('#firstName').value;
-                const lastName = document.querySelector('#lastName').value;
-                const email = document.querySelector('#email').value;
-                const password = document.querySelector('#password').value;
-                const confirmPassword = document.querySelector('#confirmPassword').value;
-
-
-                // Kiểm tra xem mật khẩu và mật khẩu xác nhận có khớp không
-                if (password !== confirmPassword) {
-                    alert('Mật khẩu và mật khẩu xác nhận không khớp!');
-                    return;
-                }
-
-                // Gọi API
-                // fetch('http://localhost:3000/api/signup', {
-                // fetch('https://webtiengnhat-be.onrender.com/api/signup', {
-                fetch('http://localhost:3000/api/signup', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        firstName: firstName,
-                        lastName: lastName,
-                        email: email,
-                        password: password,
-                    }),
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log('Success:', data);
-                        $('#registerModal').modal('hide');
-
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
-                    });
-            });
-        });
-    });
-});
-
-
-$(document).ready(function () {
-    // Kiểm tra localStorage khi tải trang
     var loggedInUser = JSON.parse(localStorage.getItem('user'));
 
     if (loggedInUser) {
@@ -157,73 +67,147 @@ $(document).ready(function () {
         $('#openSigninModal').show();
     }
 
-    $('#openSigninModal').click(function () {
-        $('#modal-container').load('signIn.html #loginModal', function () {
-            $('#loginModal').modal('show');
-
-            // Thêm event listener sau khi form đăng nhập được tải và hiển thị
-            document.querySelector('#loginForm').addEventListener('submit', function (event) {
-                // Ngăn chặn hành vi mặc định của form
-                event.preventDefault();
-
-                // Lấy dữ liệu từ form
-                const email = document.querySelector('#email').value;
-                const password = document.querySelector('#password').value;
-
-                // Gọi API
-                // fetch('https://webtiengnhat-be.onrender.com/api/login', {
-                fetch('http://localhost:3000/api/login', {
-
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        email: email,
-                        password: password,
-                    }),
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log('Success:', data);
-                        console.log(data.user.firstName);
-
-                        // Đóng modal sau khi đăng nhập thành công
-                        $('#loginModal').modal('hide');
-
-                        // Lưu thông tin người dùng vào localStorage
-                        localStorage.setItem('user', JSON.stringify({
-                            firstName: data.user.firstName,
-                            lastName: data.user.lastName
-                        }));
-
-                        // Cập nhật nội dung của nút #success với icon account và tên người dùng
-                        var successButton = document.getElementById('success');
-                        var textSpan = successButton.querySelector('.text');
-                        textSpan.innerHTML = `<i class="fas fa-user-circle"></i> ${data.user.firstName} ${data.user.lastName}`;
-
-                        // Hiển thị nút #success và ẩn nút #openSignupModal và #openSigninModal
-                        successButton.style.display = 'block';
-                        $('#openSignupModal').hide();
-                        $('#openSigninModal').hide();
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
-                    });
-            });
-        });
-    });
-
     // Xử lý sự kiện click vào nút đăng xuất
     $('#logoutButton').click(function () {
-        // Xóa thông tin người dùng khỏi localStorage
-        localStorage.removeItem('user');
+        localStorage.removeItem('user'); // Xóa thông tin người dùng từ localStorage
 
-        // Ẩn nút #success và hiển thị lại nút #openSignupModal và #openSigninModal
+        // Hiển thị lại nút đăng ký và đăng nhập
         $('#success').hide();
         $('#openSignupModal').show();
         $('#openSigninModal').show();
     });
+
+    // Xử lý sự kiện click vào nút mở form đăng ký từ modal đăng nhập
+    $(document).on('click', '#openSignupFromSignin', function () {
+        $('#loginModal').modal('hide');
+        $('#modal-container').load('signUp.html #registerModal', function () {
+            $('#registerModal').modal('show');
+            setupRegisterFormSubmitEvent(); // Thiết lập sự kiện submit cho form đăng ký
+        });
+    });
+
+    // Xử lý sự kiện click vào nút mở form đăng ký từ modal đăng nhập
+    $(document).on('click', '#openSignupFromSignin', function () {
+        $('#loginModal').modal('hide');
+        $('#modal-container').load('signUp.html #registerModal', function () {
+            $('#registerModal').modal('show');
+            setupRegisterFormSubmitEvent(); // Thiết lập sự kiện submit cho form đăng ký
+        });
+    });
+
+    // Xử lý sự kiện click vào nút mở form đăng nhập từ modal đăng ký
+    $(document).on('click', '#openSigninFromSignup', function () {
+        $('#registerModal').modal('hide');
+        $('#modal-container').load('signIn.html #loginModal', function () {
+            $('#loginModal').modal('show');
+            setupLoginFormSubmitEvent(); // Thiết lập sự kiện submit cho form đăng nhập
+        });
+    });
+
+    // Xử lý sự kiện click vào nút mở modal đăng ký từ ngoài
+    $('#openSignupModal').click(function () {
+        $('#modal-container').load('signUp.html #registerModal', function () {
+            $('#registerModal').modal('show');
+            setupRegisterFormSubmitEvent(); // Thiết lập sự kiện submit cho form đăng ký
+        });
+    });
+
+    // Xử lý sự kiện click vào nút mở modal đăng nhập từ ngoài
+    $('#openSigninModal').click(function () {
+        $('#modal-container').load('signIn.html #loginModal', function () {
+            $('#loginModal').modal('show');
+            setupLoginFormSubmitEvent(); // Thiết lập sự kiện submit cho form đăng nhập
+        });
+    });
+
+    // Hàm thiết lập sự kiện submit cho form đăng ký
+    function setupRegisterFormSubmitEvent() {
+        $('#registerForm').off('submit').on('submit', function (event) {
+            event.preventDefault();
+
+            const firstName = $('#firstName').val();
+            const lastName = $('#lastName').val();
+            const email = $('#email').val();
+            const password = $('#password').val();
+            const confirmPassword = $('#confirmPassword').val();
+
+            // Kiểm tra mật khẩu và mật khẩu xác nhận
+            if (password !== confirmPassword) {
+                alert('Mật khẩu và mật khẩu xác nhận không khớp!');
+                return;
+            }
+
+            // Gọi API đăng ký
+            fetch('http://localhost:3000/api/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password,
+                }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Đăng ký thành công:', data);
+                    $('#registerModal').modal('hide');
+                    // Thực hiện các hành động khác nếu cần
+
+                })
+                .catch(error => {
+                    console.error('Lỗi khi đăng ký:', error);
+                });
+        });
+    }
+
+    // Hàm thiết lập sự kiện submit cho form đăng nhập
+    function setupLoginFormSubmitEvent() {
+        $('#loginForm').off('submit').on('submit', function (event) {
+            event.preventDefault();
+
+            const email = $('#email').val();
+            const password = $('#password').val();
+
+            // Gọi API đăng nhập
+            fetch('http://localhost:3000/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Đăng nhập thành công:', data);
+                    $('#loginModal').modal('hide');
+                    // Thực hiện các hành động khác nếu cần
+                    localStorage.setItem('user', JSON.stringify({
+                        firstName: data.user.firstName,
+                        lastName: data.user.lastName
+                    }));
+
+                    var successButton = document.getElementById('success');
+                    var textSpan = successButton.querySelector('.text');
+                    textSpan.innerHTML = `<i class="fas fa-user-circle"></i> ${data.user.firstName} ${data.user.lastName}`;
+
+                    successButton.style.display = 'block';
+                    $('#openSignupModal').hide();
+                    $('#openSigninModal').hide();
+
+                })
+                .catch(error => {
+                    console.error('Lỗi khi đăng nhập:', error);
+                });
+
+        });
+
+    }
 
 });
 
