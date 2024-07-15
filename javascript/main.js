b91cbe78ea1561761bc59b681affe8e9ece5527a
NguyenHieu09 <hieu92145@gmail.com>
2024-07-15 23:10:52 +0700

    update api for web

--- javascript/main.js
+++ javascript/main.js
@@ -10,167 +10,78 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     async function loadPostsByType(type, sectionId) {
-//         try {
-//             const response = await fetch(`http://localhost:3000/api/type/posts?type=${type}`);
-//             if (!response.ok) {
-//                 throw new Error('Network response was not ok ' + response.statusText);
-//             }
-//             const data = await response.json();
-//             const posts = data.posts;
-//             const section = document.getElementById(sectionId);
 
-//             // Clear old posts while keeping the <h1> element
-//             section.querySelectorAll('.post').forEach(post => post.remove());
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        // Fetch số lượng bài viết theo từng loại
+        // const responseCount = await fetch('http://localhost:3000/api/count-post-by-type');
+        const responseCount = await fetch('https:webtiengnhat-be.onrender.com/api/count-post-by-type')
+        const dataCount = await responseCount.json();
+        console.log(dataCount);
 
-//             posts.forEach(post => {
-//                 const postElement = document.createElement('div');
-//                 postElement.classList.add('post');
-//                 postElement.innerHTML = `
-//                     <h2 class="post-title">${post.title}</h2>
-//                     <img class="post-image" src="${post.img}" alt="${post.title}" />
-//                     <p class="post-content">${post.content}</p>
-//                 `;
-//                 section.appendChild(postElement);
-//             });
-//         } catch (error) {
-//             console.error('Error fetching posts:', error);
-//         }
-//     }
-
-//     // Load các bài post theo loại khi trang được tải
-//     loadPostsByType('KH', 'courses');
-//     loadPostsByType('DH', 'study-abroad');
-//     loadPostsByType('KS', 'engineer-news');
-//     loadPostsByType('XKLD', 'labor-export');
-//     loadPostsByType('TD', 'job-openings');
-// });
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     async function loadPostsByType(type, sectionId) {
-//         try {
-//             const response = await fetch(`http://localhost:3000/api/type/posts?type=${type}`);
-//             if (!response.ok) {
-//                 throw new Error('Network response was not ok ' + response.statusText);
-//             }
-//             const data = await response.json();
-//             const posts = data.posts.slice(0, 6); // Giới hạn số lượng bài viết là 6
-//             const section = document.getElementById(sectionId);
-
-//             // Clear old posts while keeping the <h1> element
-//             section.querySelectorAll('.post').forEach(post => post.remove());
-
-//             let row;
-//             posts.forEach((post, index) => {
-//                 if (index % 3 === 0) {
-//                     row = document.createElement('div');
-//                     row.classList.add('row');
-//                 }
-
-//                 const postElement = document.createElement('div');
-//                 postElement.classList.add('col-md-4', 'post'); // Sử dụng col-md-4 để mỗi bài post chiếm 1/3 phần của row
-//                 postElement.innerHTML = `
-//                     <h2 class="post-title">${post.title}</h2>
-//                     <img class="post-image" src="${post.img}" alt="${post.title}" />
-//                     <p class="post-content">${post.content}</p>
-//                 `;
-
-//                 row.appendChild(postElement);
-
-//                 // Nếu là bài viết cuối cùng của hàng hoặc là bài viết cuối cùng trong danh sách, thêm row vào section
-//                 if ((index + 1) % 3 === 0 || index === posts.length - 1) {
-//                     section.appendChild(row);
-//                 }
-//             });
-//         } catch (error) {
-//             console.error('Error fetching posts:', error);
-//         }
-//     }
-
-//     // Load các bài post theo loại khi trang được tải
-//     loadPostsByType('KH', 'courses');
-//     loadPostsByType('DH', 'study-abroad');
-//     loadPostsByType('KS', 'engineer-news');
-//     loadPostsByType('XKLD', 'labor-export');
-//     loadPostsByType('TD', 'job-openings');
-// });
-
-document.addEventListener('DOMContentLoaded', () => {
-    async function loadPostsByType(type, sectionId) {
-        try {
-            const response = await fetch(`http://localhost:3000/api/type/posts?type=${type}`);
-            // const response = await fetch(`https://webtiengnhat-be.onrender.com/api/type/posts?type=${type}`);
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            const data = await response.json();
-            const posts = data.posts.slice(0, 12); // Giới hạn số lượng bài viết là 12
-
-            // Lấy phần tử cha của .posts-container trong section tương ứng
-            const section = document.getElementById(sectionId);
-            const postsContainer = section.querySelector('.posts-container');
-
-            postsContainer.innerHTML = '';
-
-            posts.forEach(post => {
-                const postHTML = `
-                    <a href="post-detail.html?id=${post.id}"  style="text-decoration: none; color: black">
-                        <div class="post">
-                            <h2 class="post-title">${post.title}</h2>
-                            <img class="post-image" src="${post.img}" alt="post-image" />
-                            <p class="post-content">${post.content}</p>
-                        </div>
-                    </a>
-                `;
-
-                postsContainer.insertAdjacentHTML('beforeend', postHTML);
-            });
-
-        } catch (error) {
-            console.error('Error fetching posts:', error);
+        if (dataCount.counts && Array.isArray(dataCount.counts)) {
+            updateButtonContent(dataCount.counts);
         }
-    }
 
-    // Load các bài post theo loại khi trang được tải
-    loadPostsByType('KH', 'courses');       // 'KH' là khóa học
-    loadPostsByType('DH', 'study-abroad');  // 'DH' là tin du học
-    loadPostsByType('KS', 'engineer-news'); // 'KS' là tin kỹ sư
-    loadPostsByType('XKLD', 'labor-export');// 'XKLD' là tin xuất khẩu lao động
-    loadPostsByType('TD', 'job-openings');  // 'TD' là tin tuyển dụng
+        // Load các bài post theo từng loại khi trang được tải
+        await loadPostsByType('KH', 'courses');       // 'KH' là khóa học
+        await loadPostsByType('DH', 'study-abroad');  // 'DH' là tin du học
+        await loadPostsByType('KS', 'engineer-news'); // 'KS' là tin kỹ sư
+        await loadPostsByType('XKLD', 'labor-export');// 'XKLD' là tin xuất khẩu lao động
+        await loadPostsByType('TD', 'job-openings');  // 'TD' là tin tuyển dụng
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 });
 
+async function updateButtonContent(counts) {
+    counts.forEach(item => {
+        const type = item.type;
+        const count = item.total_posts;
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const type = 'KH'; // Thay đổi 'courses' thành loại bài post bạn muốn lấy
-
-//     // Gọi API từ backend để lấy danh sách bài post theo 'type'
-//     fetch(`http://localhost:3000/api/type/posts?type=${type}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             const posts = data.posts; // Lấy danh sách bài post từ dữ liệu nhận được
-
-//             const postsContainer = document.querySelector('.posts-container');
-
-//             // Xóa nội dung hiện tại của postsContainer (nếu có)
-//             postsContainer.innerHTML = '';
-
-//             // Duyệt qua danh sách bài post và tạo HTML để hiển thị lên giao diện
-//             posts.forEach(post => {
-//                 const postHTML = `
-//                     <div class="post">
-//                         <h2 class="post-title">${post.title}</h2>
-//                         <img class="post-image" src="${post.img}" alt="post-image" />
-//                         <p class="post-content">${post.content}</p>
-//                     </div>
-//                 `;
-//                 postsContainer.insertAdjacentHTML('beforeend', postHTML);
-//             });
-//         })
-//         .catch(error => {
-//             console.error('Error fetching posts:', error);
-//             // Xử lý lỗi khi gọi API không thành công
-//         });
-// });
+        // Sử dụng querySelector để lấy nút button và cập nhật nội dung
+        const button = document.querySelector(`#btn-${type.toLowerCase()}`);
+        if (button) {
+            button.textContent = `Thêm mới bài viết ${type} (${count})`;
+        } else {
+            console.error(`Button #btn-${type.toLowerCase()} not found`);
+        }
+    });
+}
 
 
+async function loadPostsByType(type, sectionId) {
+    try {
+        // const response = await fetch(`http://localhost:3000/api/type/posts?type=${type}`);
+        const response = await fetch(`https://webtiengnhat-be.onrender.com/api/type/posts?type=${type}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        const posts = data.posts.slice(0, 12); // Giới hạn số lượng bài viết là 12
+
+        // Lấy phần tử cha của .posts-container trong section tương ứng
+        const section = document.getElementById(sectionId);
+        const postsContainer = section.querySelector('.posts-container');
+
+        postsContainer.innerHTML = '';
+
+        posts.forEach(post => {
+            const postHTML = `
+                <a href="post-detail.html?id=${post.id}" style="text-decoration: none; color: black">
+                    <div class="post">
+                        <h2 class="post-title">${post.title}</h2>
+                        <img class="post-image" src="${post.img}" alt="post-image" />
+                        <p class="post-content">${post.content}</p>
+                    </div>
+                </a>
+            `;
+
+            postsContainer.insertAdjacentHTML('beforeend', postHTML);
+        });
+
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+    }
+}
--- javascript/script.js
+++ javascript/script.js
@@ -151,8 +151,8 @@ $(document).ready(function () {
             }
 
             // Call the signup API
-            fetch('http://localhost:3000/api/signup', {
-                // fetch('https://webtiengnhat-be.onrender.com/api/signup', {
+            // fetch('http://localhost:3000/api/signup', {
+            fetch('https://webtiengnhat-be.onrender.com/api/signup', {
 
                 method: 'POST',
                 headers: {
@@ -198,8 +198,8 @@ $(document).ready(function () {
             }
 
             // Call login API
-            fetch('http://localhost:3000/api/login', {
-                // fetch('https://webtiengnhat-be.onrender.com/api/login', {
+            // fetch('http://localhost:3000/api/login', {
+            fetch('https://webtiengnhat-be.onrender.com/api/login', {
 
                 method: 'POST',
                 headers: {
@@ -235,7 +235,7 @@ $(document).ready(function () {
                     $('#openSignupModal').hide();
                     $('#openSigninModal').hide();
                     if (data.user.role === 'admin') {
-                        window.location.href = 'sidebar.html';
+                        window.location.href = 'admin.html';
                     }
                     if (data.user.role === 'user') {
                         window.location.href = 'home.html';
