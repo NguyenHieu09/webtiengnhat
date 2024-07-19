cf682fd0a8b49848b0b58fbc5d2e5c93c1bf977d
NguyenHieu09 <hieu92145@gmail.com>
2024-07-19 17:51:37 +0700

    fix ui admin page

--- javascript/main.js
+++ javascript/main.js
@@ -11,38 +11,38 @@ function closeNav() {
 }
 
 
-document.addEventListener("DOMContentLoaded", async () => {
-    try {
-        // Fetch số lượng bài viết theo từng loại
-        // const responseCount = await fetch('http://localhost:3000/api/count-post-by-type');
-        const responseCount = await fetch('https:webtiengnhat-be.onrender.com/api/count-post-by-type')
-        const dataCount = await responseCount.json();
-        console.log(dataCount);
+// document.addEventListener("DOMContentLoaded", async () => {
+//     try {
+//         // Fetch số lượng bài viết theo từng loại
+//         // const responseCount = await fetch('http://localhost:3000/api/count-post-by-type');
+//         const responseCount = await fetch('https:webtiengnhat-be.onrender.com/api/count-post-by-type')
+//         const dataCount = await responseCount.json();
+//         console.log(dataCount);
 
-        if (dataCount.counts && Array.isArray(dataCount.counts)) {
-            updateButtonContent(dataCount.counts);
-        }
+//         if (dataCount.counts && Array.isArray(dataCount.counts)) {
+//             updateButtonContent(dataCount.counts);
+//         }
 
-        // Load các bài post theo từng loại khi trang được tải
-        await loadPostsByType('KH', 'courses');       // 'KH' là khóa học
-        await loadPostsByType('DH', 'study-abroad');  // 'DH' là tin du học
-        await loadPostsByType('KS', 'engineer-news'); // 'KS' là tin kỹ sư
-        await loadPostsByType('XKLD', 'labor-export');// 'XKLD' là tin xuất khẩu lao động
-        await loadPostsByType('TD', 'job-openings');  // 'TD' là tin tuyển dụng
+//         // Load các bài post theo từng loại khi trang được tải
+//         await loadPostsByType('KH', 'courses');       // 'KH' là khóa học
+//         await loadPostsByType('DH', 'study-abroad');  // 'DH' là tin du học
+//         await loadPostsByType('KS', 'engineer-news'); // 'KS' là tin kỹ sư
+//         await loadPostsByType('XKLD', 'labor-export');// 'XKLD' là tin xuất khẩu lao động
+//         await loadPostsByType('TD', 'job-openings');  // 'TD' là tin tuyển dụng
 
-        // Sự kiện click vào các button để chuyển hướng đến trang tạo bài viết
-        document.querySelectorAll('.new-post-btn').forEach(btn => {
-            btn.addEventListener('click', () => {
-                const type = btn.id.replace('btn-', '').toUpperCase(); // Lấy loại bài viết từ id của button và chuyển thành in hoa
-                createNewPost(type);
-            });
-        });
+//         // Sự kiện click vào các button để chuyển hướng đến trang tạo bài viết
+//         document.querySelectorAll('.new-post-btn').forEach(btn => {
+//             btn.addEventListener('click', () => {
+//                 const type = btn.id.replace('btn-', '').toUpperCase(); // Lấy loại bài viết từ id của button và chuyển thành in hoa
+//                 createNewPost(type);
+//             });
+//         });
 
 
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-});
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//     }
+// });
 
 
 // async function updateButtonContent(counts) {
@@ -96,6 +96,84 @@ document.addEventListener("DOMContentLoaded", async () => {
 //         }
 //     });
 // }
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const responseCount = await fetch('https://webtiengnhat-be.onrender.com/api/count-post-by-type');
+
+        // Kiểm tra nội dung phản hồi
+        const textResponse = await responseCount.text();
+        console.log(textResponse);
+
+        // Chuyển phản hồi thành JSON nếu nội dung không phải là HTML
+        if (responseCount.ok) {
+            const dataCount = JSON.parse(textResponse);
+            console.log(dataCount);
+
+            if (dataCount.counts && Array.isArray(dataCount.counts)) {
+                updateButtonContent(dataCount.counts);
+            }
+
+            await loadPostsByType('KH', 'courses');       // 'KH' là khóa học
+            await loadPostsByType('DH', 'study-abroad');  // 'DH' là tin du học
+            await loadPostsByType('KS', 'engineer-news'); // 'KS' là tin kỹ sư
+            await loadPostsByType('XKLD', 'labor-export');// 'XKLD' là tin xuất khẩu lao động
+            await loadPostsByType('TD', 'job-openings');  // 'TD' là tin tuyển dụng
+
+            document.querySelectorAll('.new-post-btn').forEach(btn => {
+                btn.addEventListener('click', () => {
+                    const type = btn.id.replace('btn-', '').toUpperCase();
+                    createNewPost(type);
+                });
+            });
+        } else {
+            console.error('Network response was not ok ' + responseCount.statusText);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
+
+async function loadPostsByType(type, sectionId) {
+    try {
+        const response = await fetch(`https://webtiengnhat-be.onrender.com/api/type/posts?type=${type}`);
+        const textResponse = await response.text();
+        console.log(textResponse);
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        const data = JSON.parse(textResponse);
+        const posts = data.posts.slice(0, 12);
+
+        const section = document.getElementById(sectionId);
+        const postsContainer = section.querySelector('.posts-container');
+
+        postsContainer.innerHTML = '';
+
+        posts.forEach(post => {
+            const postHTML = `
+                <div class="post">
+                    <a href="post-detail.html?id=${post.id}" class="post-detail">
+                        <h2 class="post-title">${post.title}</h2>
+                        <img class="post-image" src="${post.img}" alt="post-image" />
+                        <div class="post-content">${post.content}</div>
+                    </a>
+                    <div class="post-buttons">
+                        <button class="edit-button" onclick="editPost(${post.id})">Sửa</button>
+                        <button class="delete-button" onclick="deletePost(${post.id})">Xóa</button>
+                    </div>
+                </div>
+            `;
+
+            postsContainer.insertAdjacentHTML('beforeend', postHTML);
+        });
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+    }
+}
 
 async function updateButtonContent(counts) {
     // Mặc định số lượng bài viết là 0 nếu không có dữ liệu
@@ -156,46 +234,46 @@ async function updateButtonContent(counts) {
 
 
 
-async function loadPostsByType(type, sectionId) {
-    try {
-        // const response = await fetch(`http://localhost:3000/api/type/posts?type=${type}`);
-        const response = await fetch(`https://webtiengnhat-be.onrender.com/api/type/posts?type=${type}`);
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        const data = await response.json();
-        const posts = data.posts.slice(0, 12); // Giới hạn số lượng bài viết là 12
+// async function loadPostsByType(type, sectionId) {
+//     try {
+//         // const response = await fetch(`http://localhost:3000/api/type/posts?type=${type}`);
+//         const response = await fetch(`https://webtiengnhat-be.onrender.com/api/type/posts?type=${type}`);
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok ' + response.statusText);
+//         }
+//         const data = await response.json();
+//         const posts = data.posts.slice(0, 12); // Giới hạn số lượng bài viết là 12
 
-        // Lấy phần tử cha của .posts-container trong section tương ứng
-        const section = document.getElementById(sectionId);
-        const postsContainer = section.querySelector('.posts-container');
+//         // Lấy phần tử cha của .posts-container trong section tương ứng
+//         const section = document.getElementById(sectionId);
+//         const postsContainer = section.querySelector('.posts-container');
 
-        postsContainer.innerHTML = '';
+//         postsContainer.innerHTML = '';
 
-        posts.forEach(post => {
-            const postHTML = `
-               
-                   <div class="post">
-                    <a href="post-detail.html?id=${post.id}" class="post-detail">
-                        <h2 class="post-title">${post.title}</h2>
-                        <img class="post-image" src="${post.img}" alt="post-image" />
-                        <div class="post-content">${post.content}</div>
-                    </a>
-                    <div class="post-buttons">
-                        <button class="edit-button" onclick="editPost(${post.id})">Sửa</button>
-                        <button class="delete-button" onclick="deletePost(${post.id})">Xóa</button>
-                    </div>
-                </div>
+//         posts.forEach(post => {
+//             const postHTML = `
 
-            `;
+//                    <div class="post">
+//                     <a href="post-detail.html?id=${post.id}" class="post-detail">
+//                         <h2 class="post-title">${post.title}</h2>
+//                         <img class="post-image" src="${post.img}" alt="post-image" />
+//                         <div class="post-content">${post.content}</div>
+//                     </a>
+//                     <div class="post-buttons">
+//                         <button class="edit-button" onclick="editPost(${post.id})">Sửa</button>
+//                         <button class="delete-button" onclick="deletePost(${post.id})">Xóa</button>
+//                     </div>
+//                 </div>
 
-            postsContainer.insertAdjacentHTML('beforeend', postHTML);
-        });
+//             `;
 
-    } catch (error) {
-        console.error('Error fetching posts:', error);
-    }
-}
+//             postsContainer.insertAdjacentHTML('beforeend', postHTML);
+//         });
+
+//     } catch (error) {
+//         console.error('Error fetching posts:', error);
+//     }
+// }
 
 function createNewPost(type) {
 
